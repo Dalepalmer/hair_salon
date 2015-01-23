4e8abfb6c8b933a6a2be11b4cac2637768124cb8
dale <student@epicodus.com>
2015-01-23 15:12:11 -0800

    run on sinatra in a barebones state

---
+++ app.rb
@@ -0,0 +1,37 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload("lib/**/*.rb")
+require('./lib/client')
+require('./lib/stylist')
+require('pg')
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get("/") do
+  @clients = Client.all()
+  @stylists = Stylist.all()
+  erb(:index)
+end
+
+post("/clients") do
+  name = params.fetch("name")
+  client_id = params.fetch("client_id").to_i()
+  client = Client.new({:name => name, :client_id => client_id,})
+  client.save()
+  @stylist = Stylist.find(client_id)
+  erb(:stylist)
+end
+
+
+post("/stylist") do
+  name = params.fetch("name")
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
+end
+
+get("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:stylist)
+end
